9514138284199b751362609edf4b2391fb4c3bec
Алексей Жданов <zhdanoff.alexey@gmail.com>
2022-04-10 21:52:16 +0300

    add seed mock

---
+++ contracts/mocks/BptVesting1SeedMock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+pragma experimental ABIEncoderV2;
+
+import "../BptVesting1Seed.sol";
+
+contract BptVesting1SeedMock is BptVesting1Seed {
+    constructor(
+        address _token,
+        bytes32 _mercleRoot,
+        uint256 _tgeTimestamp,
+        uint256 steps
+    ) public BptVesting1Seed(_token, _mercleRoot, _tgeTimestamp) {
+        categories[CategoryNames.SALE_CATEGORY] = CategoryType({
+            totalSteps: steps,
+            stepTime: 7 days,
+            tgePercent: 75000,
+            stepStageDelay: 120 days,
+            stepPercent: 10000
+        });
+    }
+}
